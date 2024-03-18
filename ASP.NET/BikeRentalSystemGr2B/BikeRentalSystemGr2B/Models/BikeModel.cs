be751e4bbdfb476b404b5756b87745a70bcff928
Rucol <rucki12@interia.pl>
2024-03-18 16:43:47 +0100

    Lab1

---
+++ ASP.NET/BikeRentalSystemGr2B/BikeRentalSystemGr2B/Models/BikeModel.cs
@@ -0,0 +1,18 @@
+﻿using BikeRentalSystemGr2B.ViewModels;
+
+namespace BikeRentalSystemGr2B.Models
+{
+    public class BikeModel
+    {
+        public int Id { get; set; }
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public BikeTypeModel BikeTypeModel { get; set; }
+        public int NumberOfBikes { get; set; }
+        public int NumberOfGears { get; set; }
+
+        
+
+    }
+}
